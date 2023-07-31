cae434d846eeb4cc77abf1f496e4bd12340e2824
Jatan Dudeja <dudeja.jatan04@gmail.com>
2023-07-31 21:01:48 +0530

    project-done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,28 @@ import starWarsLogo from '../assets/star-wars-logo.svg'
 import { useGlobalContext } from '../Context'
 
 
-export default function Header(){
+export default function Header() {
 
     const [search, setSearch] = React.useState("")
 
-    const {showSearch, tellPage} = useGlobalContext()
+    const { showSearch, tellPage, menuBar, showMenuBar } = useGlobalContext()
 
-    function setSearchFunction(e){
+    function setSearchFunction(e) {
         console.log(e.target.value)
         setSearch(e.target.value)
     }
 
-    return(
-        <header className='header-container'>
-            <Link  to='/'><img src={starWarsLogo} alt='star-wars-logo' onClick={() => tellPage('home')}/></Link>
+    return (
+        <header className=''>
+            <div className='header-container'>
+
+            <Link to='/'><img src={starWarsLogo} alt='star-wars-logo' onClick={() => tellPage('home')} /></Link>
             {/* <input  */}
             {showSearch && <form>
-                <input className='search' type='text' placeholder='Search' value={search} onChange={setSearchFunction}/>
+                <input className='search' type='text' placeholder='Search' value={search} onChange={setSearchFunction} />
             </form>}
+            </div>
+            {/* <Menu /> */}
         </header>
     )
 }
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -3,23 +3,56 @@ import folderIcon from '../assets/folder-icon.svg'
 import dropDown from '../assets/dropdown-icon.svg'
 import { useGlobalContext } from '../Context'
 import DetailSideBarPage from './Peoples/DetailSideBarPage'
+import reelIcon from '../assets/people-icon.svg'
+import closeIcon from '../assets/closeMenuIcon.svg'
+
+
+
 
 
 export default function People() {
 
-    const { tellPage, showSideBar } = useGlobalContext()
+    const { tellPage, showSideBar, peopleData, activeStyles, showDetailSideBar } = useGlobalContext()
 
-    return(
+    return (
         <div>
-        { showSideBar.peoplePage && <DetailSideBarPage />}
+            {showSideBar.peoplePage && <DetailSideBarPage />}
 
-        <div className='container-outer' onClick={() => tellPage('people')}>
-            <div className='container-inner'>
-                <img src={folderIcon} alt='films-icon' />
-                <span>People</span>
+            <div className='container-outer' style={activeStyles.people ? {background : "#CB1A80", borderRadius : '6px'} : null} onClick={() => tellPage('people')}>
+                <div className='container-inner'>
+                    <img src={folderIcon} alt='films-icon' />
+                    <span className='remove'>People</span>
+                </div>
+                <img src={activeStyles.people ? closeIcon : dropDown} alt='films-icon' />
+
             </div>
-            <img src={dropDown} alt='films-icon' />
-        </div>
+
+
+            {activeStyles.people && <div>
+
+                {
+                    peopleData.map((people) => {
+                        return (
+                            <div>
+                                <div className='container-outer child' onClick={() => showDetailSideBar(people.height, 'people')}>
+                                    <div className='container-inner'>
+                                        <img src={reelIcon} alt='films-icon' />
+                                        <span className='remove'>{people.name}</span>
+                                    </div>
+                                    <img src={dropDown} alt='films-icon' />
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
+            </div>}
+
+
+
+
+
+
         </div>
     )
 }
